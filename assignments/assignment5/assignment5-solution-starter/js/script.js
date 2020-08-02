22128363288225047b98f7e411ffe9db2764fabc
Favour Max-Oti <maxotif@gmail.com>
2020-08-02 16:30:36 +0100

    finished assignment and tidied up! I'm badass!

--- assignments/assignment5/assignment5-solution-starter/js/script.js
+++ assignments/assignment5/assignment5-solution-starter/js/script.js
@@ -107,12 +107,6 @@ function buildAndShowHomeHTML (categories) {
 
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
-      // var mainContent = 'div#main-content';
-      // insertHtml(mainContent, homeHtml);
-
-      //pass categoryshort name to menu item function
-      //dc.loadMenuItems(chosenCategoryShortName);
-
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
